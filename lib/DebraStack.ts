2646e7b28e43d823132f4b01ed736aac551fba1e
Javier Godoy <rotvulpix@gmail.com>
2023-10-14 02:15:03 -0400

    wip: Generating audio from title first, then article

--- lib/DebraStack.ts
+++ lib/DebraStack.ts
@@ -13,39 +13,56 @@ import { buildArticlesTable } from './database/articles';
 import { buildPollyLambda } from './functions/polly';
 import { buildTriggerLambda } from './functions/trigger';
 import { buildMonitorLambda } from './functions/monitor';
-
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 export class DebraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const bucket = buildBucket(this);
+    const stateMachineRole = new Role(this, 'StateMachineRole', {
+      assumedBy: new ServicePrincipal('states.amazonaws.com'),
+    });
+    const pollyManagedPolicy = ManagedPolicy.fromAwsManagedPolicyName(
+      'AmazonPollyFullAccess'
+    );
+    stateMachineRole.addManagedPolicy(pollyManagedPolicy);
 
-    // const queryTask = new LambdaInvoke(this, 'ArticlesToSpeechQueryTask', {
-    //   lambdaFunction: triggerLambda,
-    // });
+    const stateTemplate = {
+      StartAt: 'StartSpeechSynthesisTask',
+      States: {
+        StartSpeechSynthesisTask: {
+          Type: 'Task',
+          End: true,
+          Parameters: {
+            OutputS3BucketName: bucket.bucketName,
+            Text: '',
+            OutputFormat: 'mp3',
+            OutputS3KeyPrefix: 'audio/',
+            VoiceId: 'Matthew',
+          },
+          Resource: 'arn:aws:states:::aws-sdk:polly:startSpeechSynthesisTask',
+          ResultSelector: {
+            "Text.$": "$.title"
+          }
+        },
+      },
+    };
 
-    // const pollyTask = new LambdaInvoke(this, 'ArticlesToSpeechPollyTask', {
-    //   lambdaFunction: pollyLambda,
-    // });
-
-    const startState = new Pass(this, 'ArticlesToSpeechStartState');
     const stateMachine = new StateMachine(
       this,
       'ArticlesToSpeechStateMachine',
       {
-        definitionBody: DefinitionBody.fromChainable(
-          new Wait(this, 'Hello', {
-            time: WaitTime.duration(Duration.seconds(10)),
-          })
-        ),
+        definitionBody: DefinitionBody.fromString(JSON.stringify(stateTemplate)),
+        role: stateMachineRole,
       }
     );
-
+    bucket.grantReadWrite(stateMachine);
 
     const articlesTable = buildArticlesTable(this);
     const triggerLambda = buildTriggerLambda(this, articlesTable, stateMachine);
     const monitorLambda = buildMonitorLambda(this, articlesTable);
     const pollyLambda = buildPollyLambda(this);
 
-    const bucket = buildBucket(this);
   }
 }
--- lib/assets/bucket.ts
+++ lib/assets/bucket.ts
@@ -1,9 +1,10 @@
 import { RemovalPolicy, Stack } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 
 const buildBucket = (stack: Stack) => {
   const bucket = new Bucket(stack, 'ArticlesToSpeechBucket', {
     removalPolicy: RemovalPolicy.DESTROY,
+    publicReadAccess: true
   });
   return bucket;
 };
